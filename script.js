d6f6e180e606a52dfb8c44b1c8bcd66f4ffdd7af
kmr22live <kmr22live@gmail.com>
2024-05-06 16:47:51 +0530

    css changes and local storage name change

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 // Function to calculate percentage and update result
 function calculatePercentage() {
-    console.log('hai');
     let pixelInput = parseFloat(document.getElementById('pixelInput').value);
     let viewportWidth = parseFloat(document.getElementById('viewportWidthInput').value);
     let percentage = ((pixelInput / viewportWidth) * 100) + 0.001;
-    console.log(percentage);
     document.getElementById('percentageResult').textContent = 'Percentage: ' + percentage.toFixed(3) + '%';
     
     // Store input values in local storage
-    localStorage.setItem('pixelInput', pixelInput);
-    localStorage.setItem('viewportWidthInput', viewportWidth);
+    localStorage.setItem('pixToPerPixelInput', pixelInput);
+    localStorage.setItem('pixToPerViewportWidthInput', viewportWidth);
 }
 
 // Function to load input values from local storage
 function loadInputValues() {
-    let pixelInput = localStorage.getItem('pixelInput');
-    let viewportWidth = localStorage.getItem('viewportWidthInput');
+    let pixelInput = localStorage.getItem('pixToPerPixelInput');
+    let viewportWidth = localStorage.getItem('pixToPerViewportWidthInput');
     
     if (pixelInput !== null) {
         document.getElementById('pixelInput').value = pixelInput;
